db7a40a27a73bc2914e85e78aa5c0b7cfc1dff84
JrValerio <amarovsjr81@gmail.com>
2025-08-02 23:55:30 -0300

    refactor: aplica fontes, estilos globais e acessibilidade no layout.tsx

--- linkando-frontend/src/app/layout.tsx
+++ linkando-frontend/src/app/layout.tsx
@@ -1,20 +1,24 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
+// Fontes customizadas
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ['latin'],
+  variable: '--font-geist-sans',
+  display: 'swap',
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+  display: 'swap',
 });
 
+// Metadados globais
 export const metadata: Metadata = {
-  title: "Linkando.dev",
-  description: "Este é o site do Linkando.dev",
+  title: 'Linkando.dev',
+  description: 'Este é o site oficial do encurtador de URLs Linkando.dev',
 };
 
 export default function RootLayout({
@@ -23,10 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="pt-br" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="antialiased bg-gray-950 text-white min-h-screen">
         {children}
       </body>
     </html>
